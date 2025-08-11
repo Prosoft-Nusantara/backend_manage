d4391fd558bb85ad7870bd95c9401cf6ee4ec826
bagusuntoro <untorobagus65@gmail.com>
2025-08-11 21:11:56 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,10 +11,10 @@ class DashboardController extends Controller
     {
         // Total semua proyek
         $projectCount = [
-            'jumlah_proyek' => Project::count(),
-            'total_nilai_kontrak' => Project::sum('total_nilai_kontrak'),
-            'realisasi_budget' => Project::sum('realisasi_budget'),
-            'margin' => Project::sum('total_nilai_kontrak') - Project::sum('realisasi_budget')
+            'jumlah_proyek' => Project::where('status', '!=', '3')->count(),
+            'total_nilai_kontrak' => Project::where('status', '!=', '3')->sum('total_nilai_kontrak'),
+            'realisasi_budget' => Project::where('status', '!=', '3')->sum('realisasi_budget'),
+            'margin' => Project::where('status', '!=', '3')->sum('total_nilai_kontrak') - Project::where('status', '!=', '3')->sum('realisasi_budget')
         ];
 
         // Proyek Ongoing (status = 0)
